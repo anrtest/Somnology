dfc9fc3a651a80294dba7018f7d620cfd7052b95
shankitk <shankitk@kent.co.in>
2018-09-29 17:13:47 +0530

    commit 1

--- app/src/main/java/com/carobar/somnology/shankit/Main2Activity.java
+++ app/src/main/java/com/carobar/somnology/shankit/Main2Activity.java
@@ -1,11 +1,16 @@
 package com.carobar.somnology.shankit;
 
 import android.app.Activity;
+import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.carobar.somnology.R;
 import com.carobar.somnology.shankit.adapter.HourSelectionRecyclerAdapter;
@@ -13,11 +18,13 @@ import com.carobar.somnology.shankit.adapter.HourSelectionRecyclerAdapter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends Activity {
 
     private RecyclerView recyclerView;
     private HourSelectionRecyclerAdapter adapter;
     private Activity activity;
+    private TextView title;
+    private Button nextBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +32,11 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         activity = Main2Activity.this;
         recyclerView = findViewById(R.id.rv_screen1);
+        title = findViewById(R.id.headerViewScreenText);
+        title.setText("Sleep Summary");
+        nextBtn = findViewById(R.id.done);
+        nextBtn.setText("Next");
+        nextBtn.setVisibility(View.VISIBLE);
         adapter = new HourSelectionRecyclerAdapter(activity);
         final LinearLayoutManager layoutManager = new LinearLayoutManager(activity);
         layoutManager.setOrientation(LinearLayout.HORIZONTAL);
